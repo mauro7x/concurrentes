c07426f12d6b3d70478df669b1896c53eaa0bf62
Mauro Parafati <mparafati@fi.uba.ar>
2021-10-23 20:00:27 -0300

    Add listening message

--- TP1/src/bin/part2.rs
+++ TP1/src/bin/part2.rs
@@ -2,8 +2,9 @@ use actix_web::{web, App, HttpResponse, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    let server = HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
+        .bind("127.0.0.1:8080")?;
+    println!("Listening on port {:?}", 8080);
+
+    server.run().await
 }
