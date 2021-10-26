67fd81028d8ba1b19d722820621c5117263b8690
Mauro Parafati <mparafati@fi.uba.ar>
2021-10-26 01:21:00 -0300

    Read requests from file (#30)
    * Add custom path file
    
    * Add file from args

--- TP1/src/bin/part1.rs
+++ TP1/src/bin/part1.rs
@@ -1,19 +1,41 @@
-use std::{error::Error, process};
+use std::{env, error::Error, process};
 
 use lib::common::{config::GeneralConfig, paths};
 
 use lib::part1::{
-    airlines, dispatcher, hotel, logger, metrics_collector, request_handler::RequestHandler,
+    airlines, dispatcher, hotel,
+    logger::{self, LoggerSender},
+    metrics_collector,
+    request_handler::RequestHandler,
 };
+
+fn get_requests_path(logger: LoggerSender) -> String {
+    let args: Vec<String> = env::args().collect();
+    match args.get(1) {
+        Some(path) => {
+            logger.send(format!("Using requests file received (path: {})", path));
+            path.to_string()
+        }
+        None => {
+            logger.send(format!(
+                "No requests file received, using default one (path: {})",
+                paths::DEFAULT_REQUESTS
+            ));
+            String::from(paths::DEFAULT_REQUESTS)
+        }
+    }
+}
 
 fn run() -> Result<(), Box<dyn Error>> {
     let GeneralConfig {
         port: _,
         logger_config,
         metrics_collector_config,
-    } = GeneralConfig::from_path(paths::GENERAL)?;
+    } = GeneralConfig::from_path(paths::GENERAL_CONFIG)?;
 
     let logger = logger::Logger::from_config(logger_config)?;
+    let path = get_requests_path(logger.get_sender());
+
     let metrics_collector =
         metrics_collector::MetricsCollector::from_config(metrics_collector_config)?;
     let airlines = airlines::from_path(paths::AIRLINES_CONFIG, logger.get_sender())?;
@@ -25,7 +47,7 @@ fn run() -> Result<(), Box<dyn Error>> {
         metrics_collector.get_sender(),
     );
 
-    dispatcher::from_path(paths::REQUESTS, &mut req_handler)?;
+    dispatcher::from_path(path, &mut req_handler)?;
 
     req_handler.join();
     logger.join();
