2d6f3fe8b9c4b2fc97974b76489a1b2a6975c491
Tomas Nocetti <tomasnocetti@MacBook-Pro-de-Tomas.local>
2021-10-26 00:30:41 -0300

    Docs

--- TP1/src/lib/part1/airlines.rs
+++ TP1/src/lib/part1/airlines.rs
@@ -1,10 +1,16 @@
+//! Airline Webservice.
 use std::{collections::HashMap, error::Error};
 
 use crate::common::config::{AirlineConfig, AirlinesConfig};
 use crate::part1::{logger::LoggerSender, webservice::WebService};
 
 pub type Airline = WebService;
+
 pub type Airlines = HashMap<String, Airline>;
+
+/// Given a String representing a system path and a sender for the Logger
+/// this method will create a map of airles that will handle each request correspondingly.
+/// Each Airline is a WebService that controls the rate limit and simulates the fetch to the hotel provider.
 
 pub fn from_path(path: &str, logger_sender: LoggerSender) -> Result<Airlines, Box<dyn Error>> {
     let mut content = Airlines::new();
