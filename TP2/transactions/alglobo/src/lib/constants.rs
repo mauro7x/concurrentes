57413e91fb8446e0797dd6c8276693660eb3e9d0
santiago.klein <santiago.klein@mulesoft.com>
2021-12-09 19:18:22 -0300

    define constants for setting env variables

---
+++ TP2/transactions/alglobo/src/lib/constants.rs
@@ -0,0 +1,6 @@
+// Env vars
+pub const SVC_HOSTNAME: &str = "SVC_HOSTNAME";
+pub const SVC_PORT: &str = "SVC_PORT";
+pub const AIRLINE_ADDR: &str = "AIRLINE_ADDR";
+pub const BANK_ADDR: &str = "BANK_ADDR";
+pub const HOTEL_ADDR: &str = "HOTEL_ADDR";
--- TP2/transactions/service/src/lib/constants.rs
+++ TP2/transactions/service/src/lib/constants.rs
@@ -1,2 +1,3 @@
 // Env vars
-pub const SVC_NAME: &str = "SVC_NAME";
+pub const SVC_HOSTNAME: &str = "SVC_HOSTNAME";
+pub const SVC_PORT: &str = "SVC_PORT";
