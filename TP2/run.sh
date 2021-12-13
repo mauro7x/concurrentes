c9312b2051aabfc72d31e365a74d71d87bc2ebe7
Mauro Parafati <mparafati@fi.uba.ar>
2021-12-13 20:38:58 -0300

    Share protocol between directory and alglobo

--- TP2/run.sh
+++ TP2/run.sh
@@ -22,9 +22,17 @@ PAYMENTS_FILE="${2:-./examples/payments.csv}"
 function copy_build_dependencies {
   # Shared files
   mkdir -p "./services/alglobo/shared" &&
+  mkdir -p "./services/directory/shared" &&
   mkdir -p "./services/generic-service/shared" &&
+
+  # Protocol: alglobo <-> generic-service
   cp ./common/alglobo_generic-service_protocol.rs ./services/alglobo/shared/alglobo_generic-service_protocol.rs &&
   cp ./common/alglobo_generic-service_protocol.rs ./services/generic-service/shared/alglobo_generic-service_protocol.rs
+
+  # Protocol: alglobo <-> directory
+  cp ./common/alglobo_directory_protocol.rs ./services/alglobo/shared/alglobo_directory_protocol.rs &&
+  cp ./common/alglobo_directory_protocol.rs ./services/directory/shared/alglobo_directory_protocol.rs
+
 }
 
 function copy_runtime_dependencies {
--- TP2/services/directory/src/lib/utils.rs
+++ TP2/services/directory/src/lib/utils.rs
@@ -1,7 +1,23 @@
-use crate::types::*;
+use crate::{node::Node, protocol::Opcode, types::*};
 
 // ----------------------------------------------------------------------------
 
 pub fn next(id: Id) -> Id {
     (id + 1) % Id::MAX
+}
+
+pub fn encode(node: &Node) -> BoxResult<Vec<u8>> {
+    let mut msg = vec![];
+    msg.extend_from_slice(&node.id.to_le_bytes());
+    msg.extend_from_slice(&node.ip.octets().to_vec());
+
+    Ok(msg)
+}
+
+pub fn msg_from(opcode: Opcode, node: &Node) -> BoxResult<Vec<u8>> {
+    let mut msg = Vec::from(opcode);
+    let mut encoded_node = encode(node)?;
+    msg.append(&mut encoded_node);
+
+    Ok(msg)
 }
