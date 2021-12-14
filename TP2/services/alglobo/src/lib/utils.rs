64769cdba5329dd9a02541b93ea8ddf5dae560a7
Mauro Parafati <mparafati@fi.uba.ar>
2021-12-14 04:30:04 -0300

    Random failures

--- TP2/services/alglobo/src/lib/utils.rs
+++ TP2/services/alglobo/src/lib/utils.rs
@@ -10,7 +10,7 @@ pub fn fail_randomly() -> BoxResult<()> {
     let coin = thread_rng().gen_range(0.0..1.0);
     match coin < failure_rate()? {
         true => {
-            println!("<BOOM>");
+            println!("<BOOM> (coin: {})", coin);
             Err("Random failure".into())
         }
         false => Ok(()),
@@ -19,7 +19,7 @@ pub fn fail_randomly() -> BoxResult<()> {
 
 fn failure_rate() -> BoxResult<f64> {
     let failure_rate = env::var(FAILURE_RATE)
-        .unwrap_or_else(|_| "0.05".to_string())
+        .unwrap_or_else(|_| "0.01".to_string())
         .parse()?;
 
     Ok(failure_rate)
